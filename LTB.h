27145a121e4997aa7b468c22fa2644a02caedb8c
LarsThomasBremnes <35937739+LarsThomasBremnes@users.noreply.github.com>
2018-03-19 10:14:56 +0100

    Add files via upload

--- LTB.h
+++ LTB.h
@@ -8,7 +8,7 @@
 #define Internal_u32 unsigned int
 #define Internal_i16 short
 #define Internal_u16 unsigned short
-#define Internal_i8  char
+#define Internal_i8	 char
 #define Internal_u8  unsigned char
 #define Internal_f32 float
 #define Internal_f64 double
@@ -49,12 +49,12 @@ enum {
 	typedef		signed		char			PW(i8);
 	typedef		unsigned	short			PW(u16);
 	typedef		signed		short			PW(i16);
-	typedef		unsigned	int			PW(u32);
-	typedef		signed		int			PW(i32);
+	typedef		unsigned	int				PW(u32);
+	typedef		signed		int				PW(i32);
 	typedef		unsigned	long long		PW(u64);
 	typedef		signed		long long		PW(i64);
-	typedef				float			PW(f32);
-	typedef				double			PW(f64);
+	typedef					float			PW(f32);
+	typedef					double			PW(f64);
 
 	// Vector types
 	struct PW(v2i) {
@@ -833,7 +833,7 @@ enum {
 	enum LTBInternal(direction) {
 		LTBInternal(D_Left) = -1, LTBInternal(D_Right) = 1
 	}; struct PW(node_direction) {
-		PW(node)*			Node;
+		PW(node)*		Node;
 		enum LTBInternal(direction)	Direction;
 	}; typedef struct PW(node_direction) PW(node_direction);
 
@@ -1026,14 +1026,20 @@ enum {
 	}
 
 	struct PW(AVLFindResult) {
-		PW(node)*	Node;
+		PW(node)*		Node;
 		Internal_u64	Index;
 	}; typedef struct PW(AVLFindResult) PW(AVLFindResult);
 
 	PW(AVLFindResult) PW(AVLFind)(type_for_node_key Key, PW(tree)* Tree) {
+		#ifdef LTB_AVL_FailQuietly
+		if (!Tree || !Tree->Root) {
+			return (PW(AVLDeleteResult)) { 0, 0 };
+		}
+		#endif 
+
 		PW(node)* Cur = Tree->Root;
 
-		for (;;) {
+		while (Cur) {
 			#ifdef LTB_AVLMacroCompare
 				enum PW(AVLCompareResult) LowerCompare, UpperCompare;
 				AVLCompare(Key, Cur->Lower, LowerCompare);
@@ -1072,6 +1078,8 @@ enum {
 				((int*) 0)[0] = 1; // FATAL ERROR
 			}
 		}
+
+		return (PW(AVLFindResult)) { 0, 0 };
 	}
 
 	Internal_u64 LTBInternal(RestoreAVLShapeDeletion)(PW(node)** ParentOfNeedsBalancing, PW(tree)* Tree);
@@ -1079,11 +1087,17 @@ enum {
 	
 
 	struct PW(AVLDeleteResult) {
-		PW(node)*		Node;
+		PW(node)*				Node;
 		PW(type_for_node_key)	Key;
 	}; typedef struct PW(AVLDeleteResult) PW(AVLDeleteResult);
 
 	PW(AVLDeleteResult) PW(AVLDelete)(PW(type_for_node_key) Key, PW(tree)* Tree) {
+
+		#ifdef LTB_AVL_FailQuietly
+		if (!Tree || !Tree->Root) {
+			return (PW(AVLDeleteResult)) { 0, 0 };
+		}
+		#endif 
 
 		PW(node)* Cur = Tree->Root;
 		Internal_u64 OldRootBalance = Tree->Root->Balance;
@@ -1102,7 +1116,7 @@ enum {
 		AVLCompare(Key, Cur->Upper, &UpperCompare);
 		#endif
 
-		for (;;) {
+		while (Cur) {
 
 			Previous[Height].Node = Cur;
 
@@ -1161,6 +1175,8 @@ enum {
 				((int*) 0)[0] = 1; // FATAL ERROR
 			}
 		}
+
+		return (PW(AVLDeleteResult)) { 0, 0 };
 
 		NodeNeedsToBeDeleted:
 
